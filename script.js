87ba2d680a19af14e3e77718667f7c0e548ecf58
AneeshRBhat <aneeshbhatr@gmail.com>
2022-04-26 00:01:15 +0530

    feat: many things

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 var btnTranslate = document.querySelector("#translate-btn")
 var txtInput = document.querySelector("#text-input")
+var outputArea = document.querySelector("#output-area")
+
+
+
+function translatonURL(userInput) {
+    var tranURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" + "?text=" + userInput
+    return tranURL
+}
+
 
 function clickHandler(){
     var inputText = txtInput.value
-    console.log(inputText)
+    
+   fetch(translatonURL(inputText))
+   .then(response => response.json())
+   .then(JSON => console.log(JSON.contents.translated))
 }
 btnTranslate.addEventListener("click", clickHandler)
