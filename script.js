773641fc013708383584491b9fac813b2cc578ca
AneeshRBhat <aneeshbhatr@gmail.com>
2022-04-25 23:21:51 +0530

    feat: input

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var btnTranslate = document.querySelector("#translate-btn")
+var txtInput = document.querySelector("#text-input")
 
 function clickHandler(){
-    console.log("Clicked!")
+    var inputText = txtInput.value
+    console.log(inputText)
 }
 btnTranslate.addEventListener("click", clickHandler)
